2eaafcb499ee63245df699a69eb578973dfa4d69
nani67 <pnani18dec@gmail.com>
2022-11-23 17:47:45 +0530

    new tasqr 2nd page

---
+++ android/app/src/main/kotlin/com/example/tasqr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tasqr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(27, 35, 53, 1),
+          leading: IconButton(
+            onPressed: () {
+              print('sandeep');
+            },
+            icon: Icon(Icons.arrow_back_ios_new),
+          ),
+          title: TextField(
+            decoration: InputDecoration(hintText: 'Add your Team here',border: OutlineInputBorder(),),
+          ),
+          actions: [
+            Padding(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Icon(Icons.person)),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Icon(Icons.notifications),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.amber,
+                  radius: 30,
+                ),
+                Column(
+                  children: [
+                    Text('Christy Brown'),
+                    Icon(Icons.arrow_downward_sharp),
+                    Icon(Icons.arrow_back_ios_new)
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
