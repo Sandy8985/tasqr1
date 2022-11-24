07eb8be0c26e96fad5a989994f2ea27105fa5ee9
nani67 <pnani18dec@gmail.com>
2022-11-24 16:41:48 +0530

    new tasqr 2nd page

--- lib/main.dart
+++ lib/main.dart
@@ -6,37 +6,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color.fromRGBO(27, 35, 53, 1),
-            leading: IconButton(
-              onPressed: () {
-                print('sandeep');
-              },
-              icon: Icon(Icons.arrow_back_ios_new),
-            ),
-            title: TextField(
-              decoration: InputDecoration(
-                hintText: 'Add your Team here',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            actions: [
-              Padding(
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  child: Icon(Icons.person)),
-              Padding(
-                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: Icon(Icons.notifications),
-              )
-            ],
+        // backgroundColor: Color.fromRGBO(27, 35, 53, 1.0),
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(27, 35, 53, 1),
+          leading: IconButton(
+            onPressed: () {
+              print('sandeep');
+            },
+            icon: Icon(Icons.arrow_back_ios_new),
           ),
-          body: Padding(
-            padding: EdgeInsets.fromLTRB(20, 30, 0, 0),
-            child: Column(
-              children: [
-                Row(
+          title: TextField(
+            decoration: InputDecoration(
+              hintText: 'Add your Team here',
+              hintStyle: TextStyle(color: Colors.white),
+              border: OutlineInputBorder(),
+
+            ),
+          ),
+          actions: [
+            Padding(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Icon(Icons.person)),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Icon(Icons.notifications),
+            )
+          ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(20, 30, 0, 0),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Row(
                   children: [
                     CircleAvatar(
                       backgroundColor: Colors.amber,
@@ -62,17 +68,211 @@ class MyApp extends StatelessWidget {
                               Transform.rotate(
                                 angle: 45,
                                 child: Icon(Icons.arrow_upward),
-                              )
+                              ),
                             ],
                           )
                         ],
                       ),
                     )
                   ],
-                )
-              ],
-            ),
-          )),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      radius: 30,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                            child: Text('Christy Brown'),
+                          ),
+                          Row(
+                            children: [
+                              Transform.rotate(
+                                angle: 45,
+                                child: Icon(Icons.arrow_downward),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(0, 0, 50, 0),
+                              ),
+                              Transform.rotate(
+                                angle: 45,
+                                child: Icon(Icons.arrow_upward),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      radius: 30,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                            child: Text('Christy Brown'),
+                          ),
+                          Row(
+                            children: [
+                              Transform.rotate(
+                                angle: 45,
+                                child: Icon(Icons.arrow_downward),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(0, 0, 50, 0),
+                              ),
+                              Transform.rotate(
+                                angle: 45,
+                                child: Icon(Icons.arrow_upward),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      radius: 30,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                            child: Text('Christy Brown'),
+                          ),
+                          Row(
+                            children: [
+                              Transform.rotate(
+                                angle: 45,
+                                child: Icon(Icons.arrow_downward),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(0, 0, 50, 0),
+                              ),
+                              Transform.rotate(
+                                angle: 45,
+                                child: Icon(Icons.arrow_upward),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      radius: 30,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                            child: Text('Christy Brown'),
+                          ),
+                          Row(
+                            children: [
+                              Transform.rotate(
+                                angle: 45,
+                                child: Icon(Icons.arrow_downward),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(0, 0, 50, 0),
+                              ),
+                              Transform.rotate(
+                                angle: 45,
+                                child: Icon(Icons.arrow_upward),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      radius: 30,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+                            child: Text('Christy Brown'),
+                          ),
+                          Row(
+                            children: [
+                              Transform.rotate(
+                                angle: 45,
+                                child: Icon(Icons.arrow_downward),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(0, 0, 50, 0),
+                              ),
+                              Transform.rotate(
+                                angle: 45,
+                                child: Icon(Icons.arrow_upward),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: ''),
+          ],
+        ),
+      ),
     );
   }
 }
